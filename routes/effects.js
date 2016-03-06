c238b159e55d2c93d8e34b164e42c71322c2644e
Takuto Sato <takuto@Tacknyan.local>
2016-03-06 17:40:51 +0900

    triggerのステータスをeffectsで返すようにした

--- routes/effects.js
+++ routes/effects.js
@@ -11,11 +11,22 @@ router.get('/', function(req, res, next) {
     for(var i = 0, len = rows.length; i < len; i++){
       totalVolume = totalVolume + rows[i].volume;
     }
-    myGlobal.connection.query('SELECT * FROM live ORDER BY id DESC LIMIT 1', function (err, rows) {
-      var status = rows[0].status;
-      res.jsonp({
-        volume: totalVolume,
-        status: status
+
+    // trigger_sensor.status: 1 -> waiting, 2 -> activated
+    myGlobal.connection.query('SELECT * FROM trigger_sensor', function (err, rows) {
+      // すべてトリガー回収した後、トリガーのステータスを落としておく
+      myGlobal.connection.query('UPDATE trigger_sensor set status = 1 WHERE status = 2', function (err, rows) {});
+      var triggerCount = 0;
+      rows.forEach(function(row){
+        row.status == 2 && triggerCount++;
+      });
+      myGlobal.connection.query('SELECT * FROM live ORDER BY id DESC LIMIT 1', function (err, rows) {
+        var status = rows[0].status;
+        res.jsonp({
+          volume: totalVolume,
+          trigger: triggerCount,
+          status: status
+        });
       });
     });
   });
