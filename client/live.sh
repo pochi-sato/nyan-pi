9a697f13b55e3006590fdf8485f3b485c49dae36
Jun Sakamoto <jun.skmt.12@gmail.com>
2016-03-07 02:40:59 +0900

    live.sh微修正

--- client/live.sh
+++ client/live.sh
@@ -8,11 +8,14 @@ while [[ true ]]; do
   ID=$(echo $TEXT | cut -d ',' -f 1)
   SCENE=$(echo $TEXT | cut -d ',' -f 2)
   STATUS=$(echo $TEXT | cut -d ',' -f 3)
+  #echo "id: ${ID} scene: ${SCENE} status: ${STATUS}"
 
   LAST_ID=$(cat live_status | cut -d ',' -f 1)
   LAST_STATUS=$(cat live_status | cut -d ',' -f 2)
+  #echo "last_id: $LAST_ID last_status: ${LAST_STATUS}"
 
   if [[ $ID > $LAST_ID ]]; then
+    echo "New Live is started!!!"
     open -a /Applications/Google\ Chrome.app https://appear.in/nyan_bunch?scene=$SCENE &
     echo "$ID,$STATUS" > live_status
   fi
